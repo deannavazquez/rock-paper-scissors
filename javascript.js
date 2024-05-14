431de7a5bce83dd22a82e207bf0fdc8177a27d06
southxpaw5 <deannavazquez5@gmail.com>
2024-05-14 15:52:33 -0500

    Create a new function named getComputerChoice
    Write the code so that getComputerChoice will randomly return one of the following string values: “rock”, “paper” or “scissors”.

---
+++ javascript.js
@@ -0,0 +1,18 @@
+// What inputs will your program have? Will the user enter data or will you get input from somewhere else?
+        // User will have to input Rock Paper or Scissors
+   
+    // What’s the desired output?
+        // the computer will randomly pick between rock paper scissors to try to beat the user input
+     
+       
+    // Given your inputs, what are the steps necessary to return the desired output?
+        // When a user picks either rock, paper or scissor
+       
+        // Computer randomly outputs rock, paper or scissor
+
+    // Create arguments [Rock>Scissors, Paper > Rock, Scissors > Paper]
+        //Whoever has the greater output wins
+
+    function getComputerChoice () {
+        let choices = ['rock', 'paper', 'scissors']
+        return choices[Math.floor(Math.random()*choices.length)];}
