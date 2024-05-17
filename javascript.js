ad84ccdbdbdd5173f72ea5e606640035b761a083
southxpaw5 <deannavazquez5@gmail.com>
2024-05-17 13:20:12 -0500

    Move each round results into 3 separate console.logs so results are on separate lines and the code is cleaner.

--- javascript.js
+++ javascript.js
@@ -38,37 +38,43 @@ let computerScore = 0;
 function playRound(humanChoice, computerChoice) {
     if (humanChoice === 'rock' && computerChoice === 'scissors') { 
         ++humanScore;
-        console.log ("Computer chose " + computerChoice + 
-        ". You win! Rock beats scissors! Your score: " + 
+        console.log ("Computer chose " + computerChoice);
+        console.log ("You win! Rock beats scissors!");
+        console.log (" Your score: " + 
         humanScore + " Computer Score: " + computerScore );
     } else if  (humanChoice === 'paper' && computerChoice === 'rock') {
         ++humanScore;
-        console.log ("Computer chose " + computerChoice + 
-        ". You win! Paper beats rock! Your score: " + 
-        humanScore + " Computer Score: " + computerScore);
+        console.log ("Computer chose " + computerChoice);
+        console.log ("You win! Paper beats rock!");
+        console.log (" Your score: " + 
+        humanScore + " Computer Score: " + computerScore );
     } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
         ++humanScore;
-        console.log ("Computer chose " + computerChoice +
-         ". You win! Scissors beats paper! Your score: " + 
-        humanScore + " Computer Score: " + computerScore); 
+        console.log ("Computer chose " + computerChoice);
+        console.log ("You win! Scissors beats paper!");
+        console.log (" Your score: " + 
+        humanScore + " Computer Score: " + computerScore );
     } else if (humanChoice === 'rock' && computerChoice === 'paper') {
         ++computerScore;
-        console.log ("Computer chose " + computerChoice + 
-        ". You lose! Paper beats rock! Your score: " + 
-        humanScore + " Computer Score: " + computerScore);
+        console.log ("Computer chose " + computerChoice);
+        console.log ("You lose! Paper beats rock!");
+        console.log (" Your score: " + 
+        humanScore + " Computer Score: " + computerScore );
     } else if (humanChoice === 'paper' && computerChoice === 'scissors') {
         ++computerScore;
-        console.log ("Computer chose " + computerChoice + 
-        ". You lose! Scissors beats paper! Your score: " + 
-        humanScore + " Computer Score: " + computerScore);
+        console.log ("Computer chose " + computerChoice);
+        console.log ("You lose! Scissors beats paper!");
+        console.log (" Your score: " + 
+        humanScore + " Computer Score: " + computerScore );
     } else if (humanChoice === 'scissors' && computerChoice === 'rock') {
         ++computerScore;
-        console.log ("Computer chose " + computerChoice + 
-        ". You Lose! Rock beats scissors! Your score: " + 
-        humanScore + " Computer Score: " + computerScore);  
+        console.log ("Computer chose " + computerChoice);
+        console.log ("You lose! Rock beats scissors!");
+        console.log (" Your score: " + 
+        humanScore + " Computer Score: " + computerScore );
     } else { 
         console.log ("Computer chose " + computerChoice + 
-        ". ]Tie. Your score: " + 
+        ". Tie. Your score: " + 
         humanScore + " Computer Score: " + computerScore);}
 }
 
