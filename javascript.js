8e736cf7d36651f1db02e1b539972529c5f9b5dd
southxpaw5 <deannavazquez5@gmail.com>
2024-07-22 18:53:07 -0500

    Make humanChoice selected by buttons.
    modify the code so that the human choice is selected by buttons and the button event calls the playRound function, you can follow these steps:
    
    Remove the getHumanChoice function since the human choice will come from button clicks.
    
    Add event listeners to the buttons to call the playRound function.

--- javascript.js
+++ javascript.js
@@ -1,24 +1,8 @@
-// What inputs will your program have? Will the user enter data or will you get input from somewhere else?
-        // User will have to input Rock Paper or Scissors
-   
-    // What’s the desired output?
-        // the computer will randomly pick between rock paper scissors to try to beat the user input
-     
-       
-    // Given your inputs, what are the steps necessary to return the desired output?
-        // When a user picks either rock, paper or scissor
-       
-        // Computer randomly outputs rock, paper or scissor
 
-    // Create arguments [Rock>Scissors, Paper > Rock, Scissors > Paper]
-        //Whoever has the greater output wins
 
-function getComputerChoice () {
-    let choices = ['rock', 'paper', 'scissors']
-    return choices[Math.floor(Math.random()*choices.length)];
-}
 
-function getHumanChoice() {
+
+/* function getHumanChoice() {
     let input = prompt("Enter Rock, Paper, or Scissors").toLowerCase().trim();
     if (!input) {
         alert("Invalid choice, please try again.");
@@ -30,10 +14,26 @@ function getHumanChoice() {
         alert("Invalid choice, please try again.");
         return getHumanChoice(); // Ask again for valid input
     }
-}
+} */
 
 let humanScore = 0;
 let computerScore = 0;
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach(button => {
+    button.addEventListener("click", function() {
+        const humanChoice = this.getAttribute("class");
+        const computerChoice = getComputerChoice();
+        playRound(humanChoice, computerChoice);
+    });
+});
+
+
+function getComputerChoice () {
+    let choices = ['rock', 'paper', 'scissors']
+    return choices[Math.floor(Math.random()*choices.length)];
+}
 
 function playRound(humanChoice, computerChoice) {
     if (humanChoice === 'rock' && computerChoice === 'scissors') { 
@@ -78,18 +78,21 @@ function playRound(humanChoice, computerChoice) {
         humanScore + " Computer Score: " + computerScore);}
 }
 
-function playGame() {
-   for (let i = 0; i < 5; i++) {
-    const humanSelection = getHumanChoice();
+
+
+
+/* function playGame(){ */
+   /* for (let i = 0; i < 5; i++) */ /* { */
+ /*    const humanSelection = getHumanChoice();
     const computerSelection = getComputerChoice();
 
 
-    playRound(humanSelection, computerSelection);
+    playRound(humanSelection, computerSelection); */
     
 
 
-    if (i === 4) { //this makes sure that the below does not trigger on every round but only at the end
-        if (humanScore > computerScore) {
+    /* if (i === 4) {  *///this makes sure that the below does not trigger on every round but only at the end
+       /*  if (humanScore > computerScore) {
             alert ("You win the game!");
         } else if (humanScore < computerScore) {
             alert ("You lose! Computer wins the game!");
@@ -97,7 +100,12 @@ function playGame() {
             alert ("It's a tie!");
         }
      }
-    }
-}
+    } */
+/* } */
 
-    playGame();
+  /*   playGame(); */
+ 
+
+
+    /* button.addEventListener("click", () => {
+        alert(button.id); */
